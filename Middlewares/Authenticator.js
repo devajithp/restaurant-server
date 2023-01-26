c53f228eb6e3db2f19a84928603deb9398af1232
Devajith <devajithmr@gmail.com>
2023-01-26 23:52:07 +0530

    trouble shooting

--- Middlewares/Authenticator.js
+++ Middlewares/Authenticator.js
@@ -7,7 +7,7 @@ exports.jwtAuthenticator=(req,res,next)=>
    
    if(!token)
    {
-    res.status(400).json({"errorMessage" :"authorisation denied because no token"})
+    res.status(400).json({"errorMessage" :`authorisation denied because no token , cookies: ${req.cookies}`})
    }
    try {
     const decoded=jwt.verify(token,jwtSecret)
