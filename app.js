fa13d426675a3de23e765c5c4814c5d2f0c8916a
Devajith <devajithmr@gmail.com>
2023-01-26 13:02:11 +0530

    syntax change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 const corsOptions ={
     origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:true          //access-control-allow-credentials:true
     
 }
 app.use(cors(corsOptions))
