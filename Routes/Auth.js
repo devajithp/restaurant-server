c005512832dd6c6b2fb9f60274070f495c8395ca
Devajith <devajithmr@gmail.com>
2023-01-27 15:01:07 +0530

    changes in pakage.json

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -5,10 +5,7 @@ const {signupController,signinController} =require("../Controllers/Auth")
  const router= express.Router()
 
 
-router.get("/",(req,res)=>
-{
-    res.send("route working")
-})
+
 router.post("/signup",signupValidator,validatorResult,signupController)
 
 router.post("/signin",signinController)
